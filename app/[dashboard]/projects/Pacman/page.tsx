095d24df155a09071e3c418ec081ac3d11875018
RobCollins <bobguy117@gmail.com>
2025-01-04 13:51:48 -0500

    Added Pacman Page
    Added Viewable Paper for Pacman Project
    
    TODO:
    - Add video or playable demo of PacMan NN
    - Formatting
    - Home Page

--- app/[dashboard]/projects/Pacman/page.tsx
+++ app/[dashboard]/projects/Pacman/page.tsx
@@ -2,68 +2,14 @@ import Image from "next/image";
 
 export default function Project_pacman() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="videos/relic_trailer"
-          alt="Trailer"
-          width={180}
-          height={180}
-          priority
-        />
-        <div style={{ textAlign: "center", marginTop: "16px" }}>
-          <a
-            href="https://github.com/bobguy117/Robert-Collins-Portfolio"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-600 hover:underline"
-          >
-            Portfolio Website
-          </a>
-          <br />
-          <a
-            href="https://github.com/bobguy117/cs5100_Final_Kanku_Collins_Wu"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-gray-800 hover:underline"
-          >
-            Pac-Man NN
-          </a>
-          <br />
-          <a
-            href="mailto:robertcollins1107@gmail.com"
-            className="text-red-600 hover:underline"
-          >
-            RELiC
-          </a>
-        </div>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+    <div className="w-screen h-screen">
+      <main>
+      <iframe 
+        src="https://drive.google.com/file/d/1BNAvWc1W5GGSM4RVri3DjDFBTIcSlthf/preview" 
+        width="640" 
+        height="480" 
+        allow="autoplay">
+      </iframe>  
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
@@ -79,7 +25,7 @@ export default function Project_pacman() {
             width={16}
             height={16}
           />
-          Learn
+          Home
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -94,7 +40,7 @@ export default function Project_pacman() {
             width={16}
             height={16}
           />
-          Examples
+          Projects
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -109,7 +55,7 @@ export default function Project_pacman() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Contact Me →
         </a>
       </footer>
     </div>
